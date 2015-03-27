2c47a37bd4ea7b246bb3b81eb598176dca284f96
paldepind <simonfv@gmail.com>
2015-03-27 12:56:00 +0100

    Remove convert function

--- functionize.js
+++ functionize.js
@@ -136,18 +136,6 @@ var methods = exports.methods = function(obj) {
   return ret;
 };
 
-var convert = exports.convert = function(obj) {
-  var ret = {};
-  var keys = Object.getOwnPropertyNames(obj);
-  for (var i = 0; i < keys.length; ++i) {
-    var key = keys[i];
-    if (typeof obj[key] === 'function') {
-      ret[key] = invoker(obj[key].length, key);
-    }
-  }
-  return ret;
-};
-
 var mapField = exports.mapFields = curryN(3, function(keys, fn, obj) {
   for (var i = 0; i < keys.length; ++i) {
     obj[keys[i]] = fn(obj[keys[i]]);
